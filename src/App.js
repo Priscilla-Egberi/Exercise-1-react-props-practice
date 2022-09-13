e0edd98305cd96802dbd5a31c87af23bc3f70f62
Priscilla <priscillaegberi@gmail.com>
2022-09-13 11:53:06 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Just learning how to use code sandbox</h2>
+      <h2>I hope i have good experiences with this application</h2>
+      <h2>I hope i have good experiences with this application</h2>
+      <h2>I hope i have good experiences with this application</h2>
     </div>
   );
 }
