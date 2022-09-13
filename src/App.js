81d31d96ed3c5ba7f91b272f7f978ec3b9c52975
Priscilla <priscillaegberi@gmail.com>
2022-09-13 12:17:55 +0000

    Update App.js and styles.css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import Contact from "./contact"
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Just learning how to use code sandbox</h2>
-      <h2>I hope i have good experiences with this application</h2>
-      <h2>I hope i have good experiences with this application</h2>
-      <h2>I hope i have good experiences with this application</h2>
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <div className="card">
+        <div className="top">
+          <h2>Beyonce</h2>
+          <img
+            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+            alt="avatar_img"
+          />
+        </div>
+        <div className="bottom">
+          <p>+123 456 789</p>
+          <p>b@beyonce.com</p>
+        </div>
+      </div>
     </div>
   );
 }
